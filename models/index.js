f43e574e53b9899d90aa61fce7dee321f3e2822a
Ngoc Huy <vongochuy2802@gmail.com>
2024-03-29 17:06:40 +0700

    create playlistModel

---
+++ models/index.js
@@ -0,0 +1,7 @@
+const Track = require("./trackModel")
+const Playlist = require("./playlistModel")
+
+module.exports = {
+    Track,
+    Playlist
+}
---
+++ models/playlistModel.js
@@ -0,0 +1,34 @@
+const { Schema, model } = require("mongoose")
+const { schema } = require("./trackModel")
+
+const playlistSchema = new Schema({
+    id: {
+        type: String,
+        required: [true, "id is required"],
+        trim: true,
+    },
+    name: {
+        type: String,
+        required: [true, "name is required"],
+        trim: true,
+    },
+    description: {
+        type: String,
+        trim: true,
+        max: 200,
+        min: 2
+    },
+    img: {
+        type: String,
+        // required: [true, "img is required"],
+        default: 'https://res.cloudinary.com/drghk9p6q/image/upload/v1674479864/Final-Project-MERN/images-orpheus/default-images/playlist_mcyltf.webp'
+    },
+    track: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'track'
+        }
+    ]
+}, { timestamps: true })
+
+module.exports = model('playlist', playlistSchema)
